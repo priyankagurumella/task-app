f68133f3b5273d7540c0a490f404cb68d3091267
Priyanka Gurumella <priyagurumella@gmail.com>
2026-05-17 16:16:57 +0530

    connect frontend to render

--- script.js
+++ script.js
@@ -1,4 +1,4 @@
-const API = 'http://localhost:5000/api';
+const API = 'https://task-app-qlf8.onrender.com/api';
 let token = localStorage.getItem('token');
 let userName = localStorage.getItem('userName');
 
